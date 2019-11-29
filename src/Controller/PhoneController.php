d83f042f34fc80f23b492e6a6343adf256642dc2
Doudou <adeldoudou1996@gmail.com>
2019-11-29 21:32:51 +0100

    p7

---
+++ src/Controller/PhoneController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Phone;
+use App\Repository\PhoneRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class PhoneController extends AbstractController
+{
+    /**
+     * @Route("/phones", name="phone_create")
+     */
+    public function createAction(Request $request, SerializerInterface $serializer)
+    {        
+        $data = $request->getContent();
+        $phone = $serializer->deserialize($data, Phone::class, 'json');
+
+        $manager = $this->getDoctrine()->getManager();
+        $manager->persist($phone);
+        $manager->flush();
+
+        return new Response('', Response::HTTP_CREATED);
+    }
+
+    /**
+     * @Route("/phones/all", name="phone_all")
+     */
+    public function showAll(SerializerInterface $serializer, PhoneRepository $repo)
+    {
+        $phones = $repo->findAll();
+
+        $data = $serializer->serialize($phones, 'json', ['groups' => 'list']);
+
+        $response = new Response($data);
+        $response->headers->set('Content-Type', 'application/json');
+
+        return $response;
+
+        return $this->render('phone/index.html.twig', [
+            'controller_name' => 'PhoneController',
+        ]);
+    }
+
+    /**
+     * @Route("/phones/{id}", name="phone_show")
+     */
+    public function showAction(Phone $phone, SerializerInterface $serializer)
+    {
+        $data = $serializer->serialize($phone, 'json', ['groups' => 'detail']);
+
+        $response = new Response($data);
+        $response->headers->set('Content-Type', 'application/json');
+
+        return $response;
+
+        return $this->render('phone/index.html.twig', [
+            'controller_name' => 'PhoneController',
+        ]);
+    }
+}
---
+++ src/Entity/Phone.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\PhoneRepository")
+ */
+class Phone
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     * @Groups({detail"})
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Groups({"list", "detail"})
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="text")
+     * @Groups({"list", "detail"})
+     */
+    private $content;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+}
